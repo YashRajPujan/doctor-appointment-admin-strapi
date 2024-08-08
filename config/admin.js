c47263b397666c53e15d8068828a2a446fb145cc
Yash Raj <yashraj@Yashs-Air-6.lan>
2024-08-08 13:52:03 +0530

    latest

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','WxCRBjqe29vRCt6E/8SHyw=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'WxCRBjqe29vRCt6E/8SHyw=='),
   },
   transfer: {
     token: {
